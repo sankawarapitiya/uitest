8241b9a3e3d591bee02dcc2f7fe02938d4edc528
Madhusanka <sankawarapitiya@gmail.com>
2023-04-18 15:08:57 +0530

    wa

--- src/app/wz1/wz1.component.ts
+++ src/app/wz1/wz1.component.ts
@@ -543,7 +543,8 @@ export class Wz1Component implements OnInit {
     this.jobCard.documents = this.documentGroup.value;
     this.jobCard.reports = this.verificationGroup.value;
     this.jobCard.approvel = this.approvalGroup.value;
-    this.jobCard = this.basicDataGroup.value
+    this.jobCard = this.basicDataGroup.value;
+    this.jobCard.completePresentage = this.percentage;
     this.onStepChange('');
 
   }
@@ -554,7 +555,8 @@ export class Wz1Component implements OnInit {
       this.basicDataGroup.disable()
       this.basic_submit_btn = false
       window.scrollTo({ top: 0 })
-      this.jobCard = this.basicDataGroup.value
+      this.jobCard = this.basicDataGroup.value;
+      this.jobCard.completePresentage = this.percentage;
     }
 
   }
